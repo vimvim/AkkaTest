5362a4be7dea4a53910da83595d1f7350ffd6da3
vim <vitalii.migov@gmail.com>
2013-12-21 21:03:42 +0200

    Current progress on the architecture prototyping

--- src/main/scala/AkkaTest.scala
+++ src/main/scala/AkkaTest.scala
@@ -1,47 +1,105 @@
-import akka.actor.{Props, Actor, ActorSystem}
+import akka.actor.{ActorRef, Props, Actor, ActorSystem}
+import akka.pattern.ask
+import akka.pattern.{ after, ask, pipe }
 import scala.concurrent.Future
 
-class Cursor {
+case class RenderedContent(mimeType:String, content:String)
 
+sealed class Response
+
+case class RedirectResponse(repository:Repository, path:String) extends Response
+
+case class ContentResponse[T](kind:String, content:T) extends Response
+
+trait Repository {
+
+  // TODO: if already complete future is not will be dispatched to another thread call but runned immedeatellty
+  // TODO: than we can use Future in other case needs to use Actor
+  def get(path:String):Future[Response]
 }
 
-class OctopusRepository {
+case class Location(repository:Repository, path:String)
 
-  def get(path:String):Future[Cursor] = {
+class OctopusRepository extends Repository {
+
+  def get(path:String):Future[Response] = {
 
   }
 }
 
-case class PresentContent(path:String)
+class RemoteRepository extends Repository {
 
-class Presenter extends Actor {
+  def get(path: String): Future[Response] = {
+
+  }
+}
+
+
+case class PresentContent(location:Location)
+
+class Presenter(presenterDispatcher:ActorRef) extends Actor {
 
   def receive: Actor.Receive = {
 
-    case PresentContent(path) =>
+    case PresentContent(location) =>
 
-      path match {
-        case "/" =>
+      // Capture sender val
+      // val sender = this.sender
 
-          val presenter1 = context.system.actorOf(Props[Presenter], "presenter")
-          presenter1 ? PresentContent("/top")
+      def handlerFuture(label:String)(response:Response) = {
 
-          val presenter2 = context.system.actorOf(Props[Presenter], "presenter")
+        case RedirectResponse(repository, url) => location.repository.get(url) map handlerFuture("")
+
+        case ContentResponse(kind, content) =>
+
+          label match {
+
+            case "" =>
+              // This is "/"
+              // Compound content. Needs to ask presenters to provide another parts.
+
+              val pFuture1 = presenterDispatcher ? PresentContent(Location(location.repository, "/top"))
+              val pFuture2 = presenterDispatcher ? PresentContent(Location(location.repository, "/content"))
+
+              // TODO: This is how to implement soft timeout for rendering using "after"
+              // For every operation we needs to have single future ith will group another two together
+              // first is the rendering future and second is timeout fallback future
+              // THINK ABOUT SETUP ANOTHER MAPPING FOR RENDERING FUTURE IN THE FALLBACK TIMEOUT FUTURE.
+              // WE CAN ADD SOME CODE WHICH WILL HANDLE RESULT AND SEND IT ASYNC ( OVER WEBSOCKET ) TO BROWSER
+              // AFTER PAGE IS RENDERED.
+              // val searchFuture = search(worktime)
+              // val fallback = after(timeout, context.system.scheduler) {
+              //   Future successful s"$worktime ms > $timeout"
+              // }
+              // Future firstCompletedOf Seq(searchFuture, fallback)
 
 
+              val futures: Seq[Future[Double]] = //...
 
-          // Compound content. Needs to ask presenters to provide another parts.
-          val greeter = Akka.actorOf(Props[Greeter], "greeter")
+              Future.fold(futures)(0.0) {_ max _} map {maxRel =>
+                println(s"Max relevance: $maxRel")
+              }
 
-        case "/top" =>
-          // Static top content
-          val repo = new OctopusRepository()
+            case "top" =>
+              // Simple static content
+
+              // TODO: Needs to create and return future here ??
+
+              RenderedContent("text", content.asInstanceOf[String])
+
+            case "remoteContent" =>
+              // This content is loaded async from remote repository
+
+              // TODO: Needs to create and return future here ??
+
+              RenderedContent("text", content.asInstanceOf[String])
 
 
-        case "/content" =>
-          // Dynamic, async loaded content
-
+          }
       }
+
+      val getFuture = location.repository.get(location.path)
+      getFuture map handlerFuture("") pipeTo sender
   }
 }
 
