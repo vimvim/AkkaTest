ab6bf2712f4ad3ad6131c61e8f9082e5beb96d15
vim <vitalii.migov@gmail.com>
2014-03-05 18:14:32 +0200

    Workshop of the FSM  + TCP IO

--- src/main/scala/AkkaFsmIOTest.scala
+++ src/main/scala/AkkaFsmIOTest.scala
@@ -1,12 +1,100 @@
+import akka.actor._
+
+import akka.io.{ IO, Tcp }
+import akka.util.{CompactByteString, ByteString}
+import java.net.InetSocketAddress
+
 /**
- * This wile is contain code for Akka FSM IO workbench.
+ * This file contain code for Akka FSM IO workbench.
+ * This workshop will help to evaluate:
+ * - How actor will receive data from network ( ie - what messages is used,... )
+ *   Used: Received(data: ByteString)
+ * - How to emulate receiving data from net ( for testing purposes )
+ * - How to catch and test output of the Actor
+ *   Actor send Write(data: ByteString, ack: Event)
+ * - How to work with the FSM
  *
- *
- *
+ * This simple Actor will:
+ * - prompt for login
+ * - grab login name
+ * - continue grabs incoming messages and send them back as "echo" until user will enter "exit"
+ * - after that Actor will be in the idle state for some time and close conn if no login name entered
  */
 
+sealed trait State
+case object Auth extends State
+case object Running extends State
 
+sealed trait Data
+case object Uninitialized extends Data
+case class SessionData(login:String) extends Data
 
-class AkkaFsmIOTest {
+class ConnHandler(connection: ActorRef, remote: InetSocketAddress) extends Actor with FSM[State, Data] with ActorLogging {
+
+  import Tcp._
+
+  // Actor terminates when connection breaks
+  context watch connection
+
+  startWith(Auth, Uninitialized)
+
+  connection ! Write(CompactByteString("Hello ! \r\nEnter login: "))
+
+  when(Auth) {
+    case Event(Received(data), Uninitialized) ⇒
+
+      stay()
+
+      goto(Running) using SessionData(data.decodeString("UTF-8"))
+  }
+
+  when(Running) {
+
+    case Event(Received(data), SessionData(_)) ⇒
+
+      val command = data.decodeString("UTF-8")
+
+      if (command=="close") {
+
+        goto(Auth) using Uninitialized
+
+      } else {
+
+        connection ! Write(CompactByteString("cmd: "+command))
+
+        stay()
+      }
+  }
+
+  // TODO: Handle PeerClosed
+
+  initialize()
+}
+
+class Server extends Actor {
+
+  import Tcp._
+  import context.system
+
+  IO(Tcp) ! Bind(self, new InetSocketAddress("localhost", 2222))
+
+  def receive = {
+    case b @ Bound(localAddress) ⇒
+    // do some logging or setup ...
+
+    case CommandFailed(_: Bind) ⇒ context stop self
+
+    case c @ Connected(remote, local) ⇒
+      val handler = context.actorOf(Props(classOf[ConnHandler], sender, remote))
+      val connection = sender
+      connection ! Register(handler)
+  }
+}
+
+object AkkaFsmIOTest extends App {
+
+  val system = ActorSystem("akkaTest")
+
+  val server = system.actorOf(Props[Server])
 
 }
