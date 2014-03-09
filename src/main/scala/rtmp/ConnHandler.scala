c32775a2c14f8f9d48e3f693cb46ab56bbfe66d8
vim <vitalii.migov@gmail.com>
2014-03-09 08:33:51 +0200

    Start implementing packet decoding

--- src/main/scala/rtmp/ConnHandler.scala
+++ src/main/scala/rtmp/ConnHandler.scala
@@ -1,9 +1,9 @@
 package rtmp
 
-import akka.actor.{ Actor, ActorRef, Props, FSM }
+import akka.actor._
 import akka.io.{ IO, Tcp }
 import scala.concurrent.duration._
-import akka.util.ByteString
+import akka.util.{CompactByteString, ByteString}
 import java.net.InetSocketAddress
 
 
@@ -18,62 +18,67 @@ import rtmp.protocol.v2.handshake.Crypto
 sealed trait State
 case object HandshakeGet extends State
 case object HandshakeConfirm extends State
+case object ReceiveHeader extends State
+case object ReceiveBody extends State
 case object Open extends State
 case object ClosedConn extends State
 
 
 sealed trait Data
-case object Uninitialized extends Data
-case object HandshakeData extends Data
+case class Handshake(buffer:ByteString) extends Data
+case class HandshakeData extends Data
 
 /**
  *
+ * TODO: Check how and where new byte buffers is created and optimize it.
  */
-class ConnHandler extends Actor with FSM[State, Data] {
+class ConnHandler(connection: ActorRef, remote: InetSocketAddress) extends Actor with FSM[State, Data] with ActorLogging  {
 
   import Tcp._
 
-  startWith(HandshakeGet, Uninitialized)
+  startWith(HandshakeGet, Handshake(CompactByteString("")))
 
   when(HandshakeGet) {
-    case Event(Received(data), Uninitialized) ⇒
+    case Event(Received(data), Handshake(accumulatedBuffer)) ⇒
 
-      // if (!data.isCompact) {
-      //  data.compact
-      // }
+      val buffer = accumulatedBuffer.concat(data)
 
-      // log.isDebugEnabled
+      processBuffer(buffer, Constants.HANDSHAKE_SIZE, (buffer, input)=>{
 
-      // TODO: Check is there are new byte buffers is created. We needs to eliminate this.
-      val compactData = data.compact
-      val input = compactData.asByteBuffer.array()
+        val handshakeType = input(0)
+        val versionByte = input(4)
 
-      val handshakeType = input(0)
-      val versionByte = input(4)
+        if (log.isDebugEnabled) {
 
-      if (log.isDebugEnabled) {
+          log.debug("Player encryption byte: {}", handshakeType)
+          log.debug("Player version byte: {}", versionByte & 0x0ff)
 
-        log.debug("Player encryption byte: {}", handshakeType)
-        log.debug("Player version byte: {}", versionByte & 0x0ff)
+          // If the 5th byte is 0 then dont generate new-style handshake
+          log.debug("Detecting flash player version {},{},{},{}", Array[Int](input(4) & 0x0ff, input(5) & 0x0ff, input(6) & 0x0ff, input(7) & 0x0ff))
+        }
 
-        // If the 5th byte is 0 then dont generate new-style handshake
-        log.debug("Detecting flash player version {},{},{},{}", Array[Int](input(4) & 0x0ff, input(5) & 0x0ff, input(6) & 0x0ff, input(7) & 0x0ff))
-      }
+        val protocol = createProtocol(handshakeType)
+        val response = protocol.handshake(input)
 
-      val protocol = createProtocol(handshakeType)
-      val response = protocol.handshake(input)
+        connection ! Write(response.serialize())
 
+        goto(HandshakeConfirm)
 
-      // TODO: How we will test this ??? Do we needs to create separated Protocol Actor ??
-      // TODO: Seems that Write is are simple message so we can expect to test it using testing framework
-      Write(response.serialize())
-
-      // stay using Todo(ref, Vector.empty)
-    goto(HandshakeConfirm)
+      }, (restBuffer)=>{
+        stay using Handshake(restBuffer)
+      })
   }
 
   when(HandshakeConfirm) {
-    case Event(Received(data), Uninitialized) ⇒ stay()
+    case Event(Received(data), Handshake) ⇒ stay()
+  }
+
+  when(ReceiveHeader) {
+    case Event(Received(data), Handshake) ⇒ stay()
+  }
+
+  when(ReceiveBody) {
+    case Event(Received(data), Handshake) ⇒ stay()
   }
 
   when(ClosedConn) {
@@ -87,6 +92,23 @@ class ConnHandler extends Actor with FSM[State, Data] {
   }
 
   initialize()
+
+  def processBuffer(buffer:ByteString, chunkSize:Int, handlerFunc: (ByteString, Array[Byte])=>State, stayFunc: (ByteString)=>State):State = {
+
+    if (buffer.length>=chunkSize) {
+
+      val split = buffer.splitAt(chunkSize)
+
+      val compactData = split._2.compact
+      val input = compactData.asByteBuffer.array()
+
+      handlerFunc(split._1, input)
+
+    } else {
+
+      stayFunc(buffer)
+    }
+  }
 
   def createProtocol(handshakeVersion:Byte):BaseProtocol = {
 
@@ -114,6 +136,30 @@ class ConnHandler extends Actor with FSM[State, Data] {
 
       case 0x06 => throw new Exception("Encrypted connections is not supported yet {}")
       case _ => throw new Exception(s"Unsupported handshake version specified $handshakeVersion ")
+    }
+  }
+
+  def getHeaderType(firstByte:Byte) = {
+
+    if ((firstByte & 0x3f) == 0) {
+      if (remaining < 2) {
+        in.position(position)
+        state.bufferDecoding(2)
+        return null
+      }
+      headerValue = (headerByte & 0xff) << 8 | (in.get & 0xff)
+      byteCount = 2
+    } else if ((firstByte & 0x3f) == 1) {
+      if (remaining < 3) {
+        in.position(position)
+        state.bufferDecoding(3)
+        return null
+      }
+      headerValue = (headerByte & 0xff) << 16 | (in.get & 0xff) << 8 | (in.get & 0xff)
+      byteCount = 3
+    } else {
+      headerValue = headerByte & 0xff
+      byteCount = 1
     }
   }
 
